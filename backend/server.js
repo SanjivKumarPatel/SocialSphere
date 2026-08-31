33ff2fecfaaebc9a7a6f861fdfd2ff8bfbe98063
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-08-31 12:59:51 +0530

    Set database connection

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,30 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 
+import connectDB from './config/db.js'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send(' 🚀 SocialSphere API is running')
+  res.send('🚀 SocialSphere API is running')
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB()
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+}
+
+startServer()
